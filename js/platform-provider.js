6e9f49a0d9df0d533375c29b20273218fd22bc5f
Ricardo de pena <ricardo@isnull.co>
2020-03-11 17:16:29 -0400

    added native window extension as an example

--- js/platform-provider.js
+++ js/platform-provider.js
@@ -1,23 +1,38 @@
+import { generateExternalWindowSnapshot, restoreExternalWindowPositionAndState } from './external-window-snapshot.js';
+
+//We have customized out platform provider to keep track of a specific notepad window.
+//Look for the "my_platform_notes.txt" file and launch it in notepad or add another external window to this array
+const externalWindowsToTrack = [
+    {
+        name: 'Notepad',
+        title: 'my_platform_notes - Notepad'
+    }
+];
+
 fin.Platform.init({
     overrideCallback: async (Provider) => {
         class Override extends Provider {
             async getSnapshot() {
                 const snapshot = await super.getSnapshot();
-                //we add host specs to the snapshot.
+
+                //we add an externalWindows section to our snapshot
+                const externalWindows = await generateExternalWindowSnapshot(externalWindowsToTrack);
                 return {
                     ...snapshot,
-                    hostSpecs: await fin.System.getHostSpecs()
+                    externalWindows
                 };
             }
 
             async applySnapshot({ snapshot, options }) {
-                //Do not apply snapshots if we have 3 or more windows.
-                const wins = await fin.Application.getCurrentSync().getChildWindows();
-                if (wins.length >= 3) {
-                    //no more windows for you.
-                    throw new Error('Max number of windows reached');
+
+                const originalPromise = super.applySnapshot({ snapshot, options });
+
+                //if we have a section with external windows we will use it.
+                if (snapshot.externalWindows) {
+                    await Promise.all(snapshot.externalWindows.map(async (i) => await restoreExternalWindowPositionAndState(i)));
                 }
-                return super.applySnapshot({ snapshot, options });
+
+                return originalPromise;
             }
         };
         return new Override();
