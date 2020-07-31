cdc9de880087fa7ad70d5be5cd61e26ec9ecf8ce
Pierre Baize <pierre@openfin.co>
2020-07-31 14:44:34 -0600

    add support for local configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const { launch } = require('hadouken-js-adapter');
+const CopyPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+
+const appJson = 'app.json';
+const localJson = 'local.json';
+const localJsonPath = './static/local.json';
+
+const manifestFile = fs.existsSync(localJsonPath) ? localJson : appJson;
+
+console.log(`Using file at ./static/${manifestFile}`);
 
 module.exports = {
     mode: 'development',
     entry: {
-        provider: './src/provider/provider.ts',
-        window: './src/window/window.ts',
+        'provider': './src/provider/provider.ts',
+        'window': './src/window/window.ts',
         'color-view': './src/color-view/color-view.js'
     },
     module: {
@@ -13,22 +23,27 @@ module.exports = {
             {
                 test: /\.[t|j]sx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+                exclude: /node_modules/
+            }
+        ]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js']
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].bundle.js'
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: '' }]
+        })
+    ],
     devServer: {
         contentBase: path.join(__dirname, 'static'),
         compress: true,
         port: 5555,
-        onListening: function () {
-            launch({ manifestUrl: 'http://localhost:5555/app.json' })
+        onListening() {
+            launch({ manifestUrl: `http://localhost:5555/${manifestFile}` });
         }
     }
-}
+};
