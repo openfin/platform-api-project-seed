685baeced4b7153063a8e88b3daea172b8eda8d0
Varun <goelvarun88@gmail.com>
2021-01-19 14:49:43 +0800

    Updated to mimic CAT impln

--- js/platform-provider.js
+++ js/platform-provider.js
@@ -1,23 +1,30 @@
 //fin.me.showDeveloperTools();
 
-(async function() {
+(async function () {
     await fin.Platform.init();
+    const button = document.querySelector('#button');
 
-    const snapshotItem = 'snapShot';
-    const defaultSnapshotUrl = 'http://localhost:5555/default-snapshot.json';
+    button.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const snapshotItem = 'snapShot';
+        const defaultSnapshotUrl = 'http://localhost:5555/default-snapshot.json';
 
-    const platform = fin.Platform.getCurrentSync();
-    const storedSnapshotValue = window.localStorage.getItem(snapshotItem);
-    const storedSnapshot = storedSnapshotValue && JSON.parse(storedSnapshotValue);
+        const platform = fin.Platform.getCurrentSync();
+        const storedSnapshotValue = window.localStorage.getItem(snapshotItem);
+        const storedSnapshot = storedSnapshotValue && JSON.parse(storedSnapshotValue);
 
-    if(storedSnapshot) {
-        console.log('applying stored snapshot', storedSnapshot);
-        return platform.applySnapshot(storedSnapshot);
-    } else {
-        const defaultSnapshotReq = await window.fetch(defaultSnapshotUrl);
-        const defaultSnapshot = await defaultSnapshotReq.json();
+        if (false) {
+            console.log('applying stored snapshot', storedSnapshot);
+            return platform.applySnapshot(storedSnapshot);
+        } else {
+            const defaultSnapshotReq = await window.fetch(defaultSnapshotUrl);
+            const defaultSnapshot = await defaultSnapshotReq.json();
 
-        console.log('applying default snapshot', defaultSnapshot);
-        return platform.applySnapshot(defaultSnapshot.snapshot);
-    }
+            fin.me.hide();
+            console.log('applying default snapshot', defaultSnapshot);
+            return platform.applySnapshot(defaultSnapshot.snapshot);
+        }
+    });
+
+
 })();
