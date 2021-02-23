9f7c38ae4d24b06d0def5264570ae20c0fc17d7b
PhilVogt <philip.vogt@openfin.co>
2021-02-23 19:20:01 +0000

    2 small changes
    - Added a default title to stop the URL showing when a tab view is created.
    - Added an artificially slow server to highlight how the view looks when waiting for the html and js to download.

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const httpServer = require('http-server');
-const path = require('path');
-const fs = require('fs');
+const connectLib = require("connect");
+const http = require("http");
+const slow = require("connect-slow");
+const { launch, connect } = require("openfin-adapter");
+const serveStatic = require("serve-static");
+const fs = require("fs");
+const path = require("path");
 
-const { launch, connect } = require('hadouken-js-adapter');
-
+const appJson = "app.json";
+const localJson = "local.json";
 const serverParams = {
-    root: path.resolve('./'),
+    root: path.resolve("./"),
     port: 5555,
     open: false,
     logLevel: 2,
-    cache: -1
+    cache: -1,
 };
-
-const appJson = 'app.json';
-const localJson = 'local.json';
-
-//If user supplied a version argument, create a new local.json file
-if(process.argv.length > 2) {
-    const manifest = require(`./${appJson}`);
-    const runtimeVersion = process.argv[2];
-
-    manifest.runtime.version = runtimeVersion;
-    fs.writeFileSync(localJson, JSON.stringify(manifest, null, 4));
-}
+//start the server
+var app = connectLib();
+app.use(
+    slow({
+        url: /\.html$/i,
+        delay: 5000,
+    })
+).use(serveStatic("./"));
 
 //If local.json exists, use it instead of app.json
 const manifestFile = fs.existsSync(localJson) ? localJson : appJson;
@@ -30,24 +30,24 @@ const manifestFile = fs.existsSync(localJson) ? localJson : appJson;
 //To Launch the OpenFin Application we need a manifestUrl.
 const manifestUrl = `http://localhost:${serverParams.port}/${manifestFile}`;
 
-//Start the server server
-const server = httpServer.createServer(serverParams);
-server.listen(serverParams.port);
-(async() => {
+console.time("Connecting to OpenFin");
+http.createServer(app).listen(5555);
+//server.listen(serverParams.port);
+(async () => {
     try {
-        console.log('Launching application from:', manifestUrl);
+        console.log("Launching application from:", manifestUrl);
         //Once the server is running we can launch OpenFin and retrieve the port.
         const port = await launch({ manifestUrl });
 
         //We will use the port to connect from Node to determine when OpenFin exists.
         const fin = await connect({
-            uuid: 'server-connection', //Supply an addressable Id for the connection
+            uuid: "server-connection", //Supply an addressable Id for the connection
             address: `ws://localhost:${port}`, //Connect to the given port.
-            nonPersistent: true //We want OpenFin to exit as our application exists.
+            nonPersistent: true, //We want OpenFin to exit as our application exists.
         });
 
         //Once OpenFin exists we shut down the server.
-        fin.once('disconnected', process.exit);
+        fin.once("disconnected", process.exit);
     } catch (err) {
         console.error(err);
     }
