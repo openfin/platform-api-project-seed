98d57a649ee7a409fade09920f5bcd5b01598d6c
PhilVogt <philip.vogt@openfin.co>
2021-10-06 11:13:50 +0100

    Added an icon example on the left of the tab header

--- js/tab-header.js
+++ js/tab-header.js
@@ -8,16 +8,21 @@ function init() {
     myLayoutContainer.addEventListener("tab-created", async (evt) => {
         debugger;
         const { tabSelector, uuid, name } = evt.detail;
-        const tabElement = document.getElementById(tabSelector);
-        const tabClose = tabElement.getElementsByClassName("lm_close_tab")[0];
 
         // You can manipulate some aspect of the DOM here aswell.
-        tabElement.style.backgroundColor = "#ff840f";
+        const tabElement = document.getElementById(tabSelector);
+        const tabClose = tabElement.getElementsByClassName("lm_close_tab")[0];
+        const tabLeft = tabElement.getElementsByClassName("lm_left")[0];
+        var image = document.createElement("img");
+        image.src = "https://www.openfin.co/favicon-32x32.png";
+        image.style.width = "14px"
+        tabLeft.appendChild(image);
 
         const targetView = await fin.View.wrap({ uuid, name });
-        const options = await targetView.getOptions();
         const info = await targetView.getInfo();
-        // This is where you can add your click event handler for the close button
+
+        // You can also add click event handler for the close button if you want to do
+        // some custom logic on the close of the tab...
         tabClose.addEventListener("click", (evt) => {
             alert(JSON.stringify(info.url));
         }, { once: true });
