fba4aeb8254e75faf2c6035ea003193c198764f0
wenjunche <wenjun@openfin.co>
2022-05-07 18:54:41 -0400

    updated devDependencies

--- test/ofrunner/test.spec.ts
+++ test/ofrunner/test.spec.ts
@@ -2,25 +2,25 @@
 import assert from 'assert';
 import 'mocha';
 
-import { OpenFinBridgeSystem, WebDriverHelper, OpenFinBridgeWorkspace } from 'openfin-test-helpers';
+import {  WebDriver } from '@openfin/automation-helpers';
 
 // declare var fin:any;
 
 describe('Click Re-Run button in Health Check page', function() {
         const healthCheckTitle = 'OpenFin Deployment Health Check';
         it(`Switch to ${healthCheckTitle}`, async () => {
-            await WebDriverHelper.waitForWindow(healthCheckTitle, 5000);
-            const title = await WebDriverHelper.getTitle();
+            await WebDriver.waitForWindow(healthCheckTitle, 5000);
+            const title = await WebDriver.getTitle();
             assert.strictEqual(title,  healthCheckTitle);
         });
 
         it('Wait for OpenFin API ready', async () => {
-            await WebDriverHelper.waitForObjectExisting('fin.desktop', 5000);
+            await WebDriver.waitForObjectExisting('fin.desktop', 5000);
         });
 
         it("Click Run Again button", async () => {
-            const rerunButton = await WebDriverHelper.findElementById("rerun");
-            client.elementClick(rerunButton);
+            const rerunButton = await WebDriver.findElementById("rerun");
+            WebDriver.elementClick(rerunButton);
         });
     });
 
@@ -29,21 +29,21 @@ describe('Close Health Check page', function() {
     const healthCheckTitle = 'OpenFin Deployment Health Check';
     const providerPageTitle = 'Platform Window Template';
     it(`Switch to ${providerPageTitle}`, async () => {
-        await WebDriverHelper.switchToWindow(providerPageTitle);
-        const title = await WebDriverHelper.getTitle();
+        await WebDriver.switchToWindow(providerPageTitle);
+        const title = await WebDriver.getTitle();
         assert.equal(title,  providerPageTitle);
     });
 
     it("Click Close tab button", async () => {
-        const lmTabs = await WebDriverHelper.findElementsByPath('//li[contains(@class, "lm_tab")]');
+        const lmTabs = await WebDriver.findElementsByPath('//li[contains(@class, "lm_tab")]');
         console.log('lmTabs', lmTabs);
         return new Promise((resolve) => {
             lmTabs.forEach(async (element) => {
-                const title = await client.getElementAttribute(element, 'title');
+                const title = await WebDriver.raw().getElementAttribute(element, 'title');
                 if (title === healthCheckTitle) {
-                    const closeDiv = await WebDriverHelper.findElementsFromElementByPath(element, '//div[@class="lm_close_tab"]');
+                    const closeDiv = await WebDriver.findElementsFromElementByPath(element, '//div[@class="lm_close_tab"]');
                     console.log('lmTabs closeDiv', closeDiv);
-                    await client.elementClick(closeDiv[0]);
+                    await WebDriver.elementClick(closeDiv[0]);
                     resolve();
                 }
             });    
@@ -54,11 +54,11 @@ describe('Close Health Check page', function() {
 describe('Close App', function() {
     const providerUrl = 'http://localhost:5555/provider.html';
     it(`Switch to ${providerUrl}`, async () => {
-        await WebDriverHelper.switchToWindowByUrl(providerUrl);
-        const url = await WebDriverHelper.getUrl();
+        await WebDriver.switchToWindowByUrl(providerUrl);
+        const url = await WebDriver.getUrl();
         assert.strictEqual(url,  providerUrl);
-        await WebDriverHelper.callMethod('fin.desktop.System.exit', undefined, false);
-        await WebDriverHelper.sleep(2000);  // pause here to give Runtime time to exit
+        await WebDriver.callMethod('fin.desktop.System.exit', undefined, false);
+        await WebDriver.sleep(2000);  // pause here to give Runtime time to exit
     });
 
 });
